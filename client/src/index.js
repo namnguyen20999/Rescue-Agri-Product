7d17b747ea0fc62cfa43b24937ca571c63c4024e
Nam <nam.nguyen@getcerebral.com>
2022-03-31 17:47:05 +0700

    Adding animation to search function

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { StyledEngineProvider } from '@mui/material/styles';
 import App from './App';
 
-ReactDOM.render(
-  <StyledEngineProvider injectFirst>
-    <App />
-  </StyledEngineProvider>,
-  document.querySelector('#root')
-);
+ReactDOM.render(<App />, document.querySelector('#root'));
--- client/src/screens/Homescreen/Homescreen.js
+++ client/src/screens/Homescreen/Homescreen.js
@@ -3,6 +3,8 @@ import products from '../../data/productData';
 import { Container, Row, Col } from 'react-bootstrap';
 import ProductCard from '../../Components/Card/ProductCard/ProductCard';
 import SearchBar from '../../Components/Navbar/SearchBar';
+import { motion } from 'framer-motion';
+import styled from 'styled-components';
 
 export default function Homescreen() {
   const [filter, setFilter] = React.useState(' ');
@@ -17,20 +19,30 @@ export default function Homescreen() {
     });
     setSearchResult(newSearchResult);
   };
+
+  const Styled = styled(motion.div)`
+    display: grid;
+    grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
+    grid-column-gap: 1rem;
+    grid-row-gap: 2rem;
+  `;
+
   return (
     <div>
       <SearchBar onSubmit={handleFiltersChange}></SearchBar>
       <Container fluid>
-        <Row>
-          {searchResult &&
-            searchResult.map((product, index) => {
-              return (
-                <Col lg={3} md={4} sm={6} key={product.id}>
-                  <ProductCard product={product} />
-                </Col>
-              );
-            })}
-        </Row>
+        <Styled animate={{ opacity: 1 }} initial={{ opacity: 0 }} exit={{ opacity: 0 }} Layout>
+          <Row>
+            {searchResult &&
+              searchResult.map((product, index) => {
+                return (
+                  <Col lg={3} md={4} sm={6} key={index}>
+                    <ProductCard product={product} />
+                  </Col>
+                );
+              })}
+          </Row>
+        </Styled>
       </Container>
     </div>
   );
