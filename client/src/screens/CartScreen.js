e5f3f050507e3e796a4ee105af10f279e2bf41c0
Nam Nguyen <namnguyen20999@gmail.com>
2022-04-15 02:52:09 +0700

    add to cart

--- client/src/screens/CartScreen.js
+++ client/src/screens/CartScreen.js
@@ -1,94 +1,108 @@
 import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
 import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
+import styled from 'styled-components/macro';
+
+const QuantityButton = styled.div`
+  border: none;
+  outline: none;
+  background: none;
+  padding: 0.7rem 1.7rem;
+  cursor: pointer;
+`;
+
+const ButtonGroup = styled.div`
+  display: flex;
+  align-items: flex-start;
+  justify-content: center;
+  width: 7.125rem;
+  max-width: 100%;
+  border: 0.5px solid rgb(177, 177, 177);
+  border-radius: 5px;
+`;
+
+const Quanity = styled.div`
+  padding: 0.7rem 0;
+`;
+
+const RemovedButton = styled.button`
+  border: none;
+  outline: none;
+  background: none;
+  margin-top: 0.7 rem;
+  cursor: pointer;
+  color: red;
+  width: fit-content;
+  padding: 0;
+  text-decoration: underline;
+`;
 
 export default function CartScreen() {
   const cart = useSelector(state => state.cartReducer);
+  const cartItems = cart.cartItems;
+  console.log(cartItems);
   return (
-    <div className="cart-container">
-      <h2>Shopping Cart</h2>
-      {cart.cartItems.length === 0 ? (
-        <div className="cart-empty">
-          <p>Your cart is currently empty</p>
-          <div className="start-shopping">
-            <Link to="/">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="16"
-                height="16"
-                fill="currentColor"
-                className="bi bi-arrow-left"
-                viewBox="0 0 16 16"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8z"
-                />
-              </svg>
-              <span>Start Shopping</span>
-            </Link>
-          </div>
-        </div>
-      ) : (
-        <div>
-          <div className="titles">
-            <h3 className="product-title">Product</h3>
-            <h3 className="price">Price</h3>
-            <h3 className="Quantity">Quantity</h3>
-            <h3 className="total">Total</h3>
-          </div>
-          <div className="cart-items">
-            {cart.cartItems?.map(cartItem => (
-              <div className="cart-item" key={cartItem.id}>
-                <div className="cart-product">
-                  <img src={cartItem.image} />
-                  <div>
-                    <h3>{cartItem.name}</h3>
-                    <p>{cartItem.description}</p>
-                    <button>Remove</button>
-                  </div>
-                </div>
-                <div className="cart-product-price">${cartItem.prices}</div>
-                <div className="cart-product-quantity">
-                  <button>-</button>
-                  <div className="count">{cartItem.cartQuantity}</div>
-                  <button>+</button>
-                </div>
-                <div className="cart-product-total-price">${cartItem.price * cartItem.cartQuantity}</div>
-              </div>
-            ))}
-          </div>
-          <div className="cart-summary">
-            <button className="clear-cart">Clear Cart</button>
-            <div className="cart-checkout">
-              <div className="subtotal">
-                <span>Subtotal</span>
-                <span className="amount">${cart.cartTotalAmount}</span>
-              </div>
-              <p>Taxes and shipping calculated at checkout</p>
-              <button>Check out</button>
-              <div className="continue-shopping">
-                <Link to="/">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="16"
-                    height="16"
-                    fill="currentColor"
-                    className="bi bi-arrow-left"
-                    viewBox="0 0 16 16"
-                  >
-                    <path
-                      fillRule="evenodd"
-                      d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8z"
-                    />
-                  </svg>
-                  <span>Continue Shopping</span>
-                </Link>
-              </div>
-            </div>
-          </div>
-        </div>
-      )}
-    </div>
+    <Container fluid>
+      <Row>
+        <Col>
+          <h1>Your cart items</h1>
+        </Col>
+      </Row>
+      <Row>
+        <Link to="/">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="16"
+            height="16"
+            fill="currentColor"
+            className="bi bi-arrow-left"
+            viewBox="0 0 16 16"
+          >
+            <path
+              fillRule="evenodd"
+              d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8z"
+            />
+          </svg>
+          <span>Continue Shopping</span>
+        </Link>
+      </Row>
+      <Container>
+        <Row>
+          <Col>Product</Col>
+          <Col>Price</Col>
+          <Col>Quantity</Col>
+          <Col>Total</Col>
+        </Row>
+        {cartItems.map(item => {
+          return (
+            <Row key={item.id}>
+              <Col>
+                <Row>
+                  <Col>
+                    <img src={item.image} style={{ width: '5rem', maxWidth: '100%' }} />
+                  </Col>
+                  <Col>
+                    <Row>{item.name}</Row>
+                    <Row>
+                      <RemovedButton>Remove</RemovedButton>
+                    </Row>
+                  </Col>
+                </Row>
+              </Col>
+              <Col>{item.prices} $</Col>
+              <Col>
+                <ButtonGroup>
+                  <QuantityButton>+</QuantityButton>
+                  <Quanity>{item.cartQuantity}</Quanity>
+                  <QuantityButton>-</QuantityButton>
+                </ButtonGroup>
+              </Col>
+              <Col>{item.prices * item.cartQuantity} $</Col>
+            </Row>
+          );
+        })}
+      </Container>
+    </Container>
   );
 }
