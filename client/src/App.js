d104eb122ec9dee09835b492454df29161b1a9eb
T1Cheby <95405716+T1Cheby@users.noreply.github.com>
2022-05-04 18:07:46 +0700

    Add form

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import bootstrap from '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import "./Components/Cart/Total";
 import { BrowserRouter, Route } from 'react-router-dom';
 import './App.css';
 import Homescreen from './screens/HomeScreen';
 import CartScreen from './screens/CartScreen';
+import AddProduct from './screens/AddProduct';
 
 export default function App() {
   return (
@@ -11,6 +13,7 @@ export default function App() {
       <BrowserRouter>
         <Route path="/" exact component={Homescreen} />
         <Route path="/cart" exact component={CartScreen} />
+        <Route path="/AddProduct" exact component={AddProduct} />
       </BrowserRouter>
     </div>
   );
