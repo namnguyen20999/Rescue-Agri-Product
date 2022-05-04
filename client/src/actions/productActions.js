16941662dcc01e39c387986427dbb3ef522522b7
T1Cheby <95405716+T1Cheby@users.noreply.github.com>
2022-05-04 17:31:21 +0700

    Update productActions.js

--- client/src/actions/productActions.js
+++ client/src/actions/productActions.js
@@ -8,3 +8,13 @@ export const getAllProduct = () => async dispatch => {
     dispatch({ type: 'GET_PRODUCT_FAILED', payload: e });
   }
 };
+export const addProduct=(product)=>async dispatch=>{
+  dispatch({type:'ADD_PRODUCT REQUEST'})
+  try {
+      const response= await axios.post('/api/products/addproducts' , {product})
+      console.log(response);
+      dispatch({type:'ADD_PRODUCT_SUCCESS'})
+  } catch (error) {
+      dispatch({type:'ADD_PRODUCT_FAILED' , payload : error})
+  }
+}
