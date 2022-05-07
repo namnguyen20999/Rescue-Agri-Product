d81715c2e2bfe18657c676abcd53521ec9b3bf9f
Thu Nguyen <hathu0610@gmail.com>
2022-05-07 17:38:12 +0700

    add css for login/register

--- client/src/screens/RegisterScreen.js
+++ client/src/screens/RegisterScreen.js
@@ -4,6 +4,8 @@ import { registerUser } from '../actions/userActions';
 import Loading from '../Components/Loading';
 import Error from '../Components/Success';
 import Success from '../Components/Success';
+import styled from 'styled-components';
+import logo from '../assets/logo_images/logo.png';
 
 export default function Registerscreen() {
   const [name, setname] = useState('');
@@ -25,24 +27,32 @@ export default function Registerscreen() {
       dispatch(registerUser(user));
     }
   }
+  const btnstyle= {
+    backgroundColor: "#3bb77e",
+    color: "white"
+
+  };
 
   return (
     <div className="register">
       <div className="row justify-content-center mt-5">
-        <div className="col-md-5 mt-5 text-left shadow-lg p-3 mb-5 bg-white rounded">
-          {loading && <Loading />}
+      <div className="d-flex flex-column align-items-center col-md-3 mt-5 text-left shadow-lg p-3 mb-5 bg-white rounded">
+      {loading && <Loading />}
           {success && <Success success="User Registered Successfully" />}
           {error && <Error error="Email already registred" />}
-
-          <h2 className="text-center m-2" style={{ fontSize: '35px' }}>
+          <img src={logo} width="100" height="100" alt="React Bootstrap logo" />
+          
+          <h2 className="text-center" style={{ fontSize: '35px' }}>
             Register
           </h2>
-          <div>
+          <h6 style={{fontWeight: 'light', textAlign:'center', fontStyle: 'italic', color: 'grey'}} className="mx-3">Register to start selling/buying agriculture products today!</h6>
+
+          <div className="d-flex flex-column align-items-center justify-content-center">
             <input
               required
               type="text"
               placeholder="name"
-              className="form-control"
+              className="form-control my-2"
               value={name}
               onChange={e => {
                 setname(e.target.value);
@@ -52,7 +62,7 @@ export default function Registerscreen() {
               required
               type="text"
               placeholder="email"
-              className="form-control"
+              className="form-control my-2"
               value={email}
               onChange={e => {
                 setemail(e.target.value);
@@ -61,7 +71,7 @@ export default function Registerscreen() {
             <input
               type="text"
               placeholder="password"
-              className="form-control"
+              className="form-control my-2"
               value={password}
               required
               onChange={e => {
@@ -71,20 +81,22 @@ export default function Registerscreen() {
             <input
               type="text"
               placeholder="confirm password"
-              className="form-control"
+              className="form-control my-2"
               value={cpassword}
               required
               onChange={e => {
                 setcpassword(e.target.value);
               }}
             />
-            <button onClick={register} className="btn mt-3 mb-3">
+            <button style={btnstyle}  onClick={register} className="btn my-2">
               REGISTER
             </button>
             <br />
-            <a style={{ color: 'black' }} href="/login">
-              Click Here To Login
+            <span className="mb-5" style={{ color: 'grey' }}>Already have an account?&nbsp;  
+            <a style={{ color: '#3bb77e' }} href="/login">
+              Login
             </a>
+            </span> 
           </div>
         </div>
       </div>
